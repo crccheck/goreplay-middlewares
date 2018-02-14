8c55bcebf6bf1fd0ccf8df60af0cb5a4628718a2
crccheck <c@crccheck.com>
2018-02-13 18:19:25 -0600

    save the response to a temp file

--- gormid.sh
+++ gormid.sh
@@ -3,10 +3,14 @@
 # Based on:
 # https://github.com/buger/goreplay/blob/master/examples/middleware/echo.sh
 
+TMP_DIR=$(mktemp -d)
+
 function log {
     # Logging to stderr, because stdout/stdin used for data transfer
     >&2 echo ">>> $1"
 }
+
+log "Sending to: $TMP_DIR"
 
 while read line; do
     decoded=$(echo -e "$line" | xxd -r -p)
@@ -14,15 +18,19 @@ while read line; do
     header=$(echo -e "$decoded" | head -n +1)
     payload=$(echo -e "$decoded" | tail -n +2)
 
+    header_bits=( $header )
+    request_id=${header_bits[1]}
+
     log ""
     log "==================================="
 
-    case ${header:0:1} in
+    case ${header_bits[0]} in
     "1")
         log "Request type: Request"
         ;;
     "2")
         log "Request type: Original Response"
+        echo "$payload" > $TMP_DIR/$request_id
         ;;
     "3")
         log "Request type: Replayed Response"
@@ -30,11 +38,11 @@ while read line; do
     *)
         log "Unknown request type $header"
     esac
+
+    # REQUIRED. Send to the next middleware/Goreplay
     echo "$line"
+    log "${header_bits[1]}"
 
     log "$header"
     log $payload
-
-    # log "Original data: $line"
-    # log "Decoded request: $decoded"
 done;
